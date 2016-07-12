7fe51e2ba89be2021538eab005aec0b1d246ba23
lvniqi <lvniqi@gmail.com>
2016-07-12 22:51:42 +0800

    添加CPP

--- stero_vision/script/cpp_speed_up/cpp_speed_up/cpp_speed_up.cpp
+++ stero_vision/script/cpp_speed_up/cpp_speed_up/cpp_speed_up.cpp
@@ -22,3 +22,24 @@ double __stdcall test2(double a[], int strides[], int shapes[]) {
 	}
 	return sum;
 }
+
+void __stdcall compute_cost_d(INT16 result[], INT16 left[], INT16 right[], int strides[], int shapes[]) {
+	//行数
+	int row_length = shapes[0];
+	//列数
+	int column_length = shapes[1];
+	//行列宽度
+	int S0 = strides[0] / sizeof(INT16);
+	int S1 = strides[1] / sizeof(INT16);
+	for (int row = 0; row < row_length; row ++) {
+		for (int column = 0; column < column_length; column ++) {
+			int pos = row*S0 + column*S1;
+			if (left[pos] > right[pos]) {
+				result[pos] = left[pos] - right[pos];
+			}
+			else {
+				result[pos] = right[pos] - left[pos];
+			}
+		}
+	}
+}
--- stero_vision/script/cpp_speed_up/cpp_speed_up/cpp_speed_up.h
+++ stero_vision/script/cpp_speed_up/cpp_speed_up/cpp_speed_up.h
@@ -2,3 +2,4 @@
 
 extern "C" _declspec(dllexport) int __stdcall test1(int a, int b);
 extern "C" _declspec(dllexport) double __stdcall test2(double a[], int strides[], int shapes[]);
+extern "C" _declspec(dllexport) void __stdcall compute_cost_d(INT16 result[], INT16 left[], INT16 right[], int strides[], int shapes[]);
